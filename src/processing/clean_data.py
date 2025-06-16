9b171a1d2414d601d7db7086f97e3829e8346a84
hmnbstn <bastien.hamon17@gmail.com>
2025-06-16 11:22:10 +0200

    Update clean_data.py

--- src/processing/clean_data.py
+++ src/processing/clean_data.py
@@ -5,5 +5,13 @@ def prepare_data(df, indicator):
         df = df[["date", "value"]].dropna()
         df["date"] = pd.to_datetime(df["date"], format="%Y")
         df = df.sort_values("date")
-        df.rename(columns={"value": indicator}, inplace=True)
+
+        if indicator in ["gdp", "trade_balance"]:
+            df[indicator] = df["value"] / 1_000_000_000  # convert to billions
+        elif indicator in ["inflation", "unemployment", "interest_rate"]:
+            df[indicator] = df["value"].round(2)
+        else:
+            df[indicator] = df["value"]
+
+        df = df[["date", indicator]]
     return df
